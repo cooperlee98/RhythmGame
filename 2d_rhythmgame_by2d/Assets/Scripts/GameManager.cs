9d93f01157c909ac5ff4f0cf13a6763674878957
cooperlee98 <cooper1998@naver.com>
2022-03-28 22:08:34 +0900

    from(3-1)to(3-3)

---
+++ 2d_rhythmgame_by2d/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; set; }//게임매니저를 싱글 톤으로 처리함 
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    public float noteSpeed;
+
+    /*
+     * bad:1
+     * good:2
+     * perfect:3
+     * miss:4
+     */
+
+    public enum judges { NONE=0, BAD,GOOD, PERFECT, MISS};
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2d_rhythmgame_by2d/Assets/Scripts/NoteBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehavior : MonoBehaviour
+{
+    public int noteType;
+    private GameManager.judges judge;
+    private KeyCode keyCode;
+
+    void Start()
+    {
+        if (noteType == 1) keyCode=KeyCode.D;
+        else if(noteType == 2) keyCode = KeyCode.F;
+        else if (noteType == 3) keyCode = KeyCode.J;
+        else if (noteType == 4) keyCode = KeyCode.K;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * GameManager.instance.noteSpeed);
+
+        if (Input.GetKey(keyCode))//사용자가 노트키를 입력하면 해당 노트에 대한 판정 진행
+        {
+            Debug.Log(judge);
+            if (judge != GameManager.judges.NONE) Destroy(gameObject);//노트가 판정 선에 닿기 시작한 이후엔 해당노트 제거
+        }
+
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="Bad Line")
+        {
+            judge = GameManager.judges.BAD;
+        }
+        else if (other.gameObject.tag == "Good Line")
+        {
+            judge = GameManager.judges.GOOD;
+        }
+        else if (other.gameObject.tag == "Perfect Line")
+        {
+            judge = GameManager.judges.PERFECT;
+        }
+        else if (other.gameObject.tag == "Miss Line")
+        {
+            judge = GameManager.judges.MISS;
+        }
+    }
+}
